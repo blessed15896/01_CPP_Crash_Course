d5a2a4295ceef761a929eeeae4475f887cc8631c
Blessed Sibanda <blessed15896@gmail.com>
2024-05-15 17:34:24 +0200

    Use streams to accept input from user

--- 01_CPP_Crash_Course.cpp
+++ 01_CPP_Crash_Course.cpp
@@ -1,15 +1,11 @@
-// helloworld 
 #include <iostream>
+#include <print>
 
-/*
-This is a multiline comment.
-The compiler will ignore it.
-*/
+
 int main()
 {
-    std::cout << "There are " << 219 << " ways I love you." << std::endl;
-    std::cout << std::format("There are {} ways I love you.", 219) << std::endl;
-    std::cout << std::format("{} + {} = {}", 2, 4, 6) << std::endl;
-    return 0;
+    int value;
+    std::cin >> value;
+    std::println("You entered {}", value);
 }
 
