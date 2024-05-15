29cc710e5e77f89460c962b5719651b39a615c0e
Blessed Sibanda <blessedsibanda.me@gmail.com>
2024-05-15 17:05:59 +0200

    Printing Text

--- 01_CPP_Crash_Course.cpp
+++ 01_CPP_Crash_Course.cpp
@@ -1,20 +1,15 @@
-// 01_CPP_Crash_Course.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// helloworld 
+#include <print>
 
-#include <iostream>
-
+/*
+This is a multiline comment.
+The compiler will ignore it.
+*/
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::println("Hello, World!");
+    std::println("There are {} ways I love you.", 219);
+    std::println("{} + {} = {}", 2, 4, 6);
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
