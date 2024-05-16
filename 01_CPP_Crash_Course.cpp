21db1f5589cf53e5cd180cb6eea7843248e47592
Blessed Sibanda <blessed15896@gmail.com>
2024-05-16 08:26:35 +0200

    Initializer lists

--- 01_CPP_Crash_Course.cpp
+++ 01_CPP_Crash_Course.cpp
@@ -1,47 +1,22 @@
 #include <print>;
-#include <array>;
+#include <initializer_list>;
 
 using namespace std;
 
+int sum(initializer_list<int> values)
+{
+    int total{ 0 };
+    for (int value : values) {
+        total += value;
+    }
+    return total;
+}
+
 int main()
 {
-    {
-        println("The while loop");
-        int i{ 0 };
-        while (i < 5)
-        {
-            println("This is silly");
-            ++i;
-        }
-    }
-    {
-        println("\nThe do/while loop");
-        int i{ 100 };
-        do
-        {
-            println("This is silly");
-            ++i;
-        } while (i < 5);
-    }
-    println("\nThe for loop");
-    {
-        for (int i{0}; i < 5; i++)
-        {
-            println("This is silly");
-        }
-    }
-    println("\nThe range-based for loop");
-    {
-        array arr{ 1,2,3,4 };
-        for (int i : arr) {
-            println("{}", i);
-        }
-    }
-    println("\nThe range-based for loop with initializer");
-    {
-        for (array arr{ 1,2,3,4 }; int i : arr) {
-            println("{}", i);
-        }
-    }
+    int a{ sum({1,2,3}) };
+    int b{ sum({10,20,30,40,50,60}) };
+    println("a = {}", a);
+    println("b = {}", b);
 }
 
