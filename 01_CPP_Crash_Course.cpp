d375d2e8f14457c7de7386db0ef666cc564ae902
Blessed Sibanda <blessed15896@gmail.com>
2024-05-16 08:08:24 +0200

    The for loop

--- 01_CPP_Crash_Course.cpp
+++ 01_CPP_Crash_Course.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     {
-        // The while loop
+        println("The while loop");
         int i{ 0 };
         while (i < 5)
         {
@@ -13,9 +13,8 @@ int main()
             ++i;
         }
     }
-    println("");
     {
-        // The do/while loop
+        println("\nThe do/while loop");
         int i{ 100 };
         do
         {
@@ -23,6 +22,13 @@ int main()
             ++i;
         } while (i < 5);
     }
-    println("");
+    println("\nThe for loop");
+    {
+        // The for loop
+        for (int i{0}; i < 5; i++)
+        {
+            println("This is silly");
+        }
+    }
 }
 
