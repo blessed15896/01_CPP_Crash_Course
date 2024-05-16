cc4a88f3c79e91380e5ef811b51aec5a1196029a
Blessed Sibanda <blessed15896@gmail.com>
2024-05-16 16:45:46 +0200

    Scope resolution

--- 01_CPP_Crash_Course.cpp
+++ 01_CPP_Crash_Course.cpp
@@ -1,15 +1,26 @@
 #include <print>
 
-import AirlineTicket;
-
 using namespace std;
+
+class Demo
+{
+public:
+	int get() { return 5; }
+};
+
+int get() { return 10; }
+
+namespace NS
+{
+	int get() { return 20; }
+}
 
 int main()
 {
-	AirlineTicket myTicket;
-	myTicket.setPassengerName("Blessed Sibanda");
-	myTicket.setNumberOfMiles(400);
-	double cost{ myTicket.calculatePriceInDollars() };
-	println("The ticket will cost ${}", cost);
+	Demo d;
+	println("{}", d.get());
+	println("{}", NS::get());
+	println("{}", ::get());
+	println("{}", get());
 }
 
