e3009f42f49937a68abd5dbda8dc2420b8327e6b
Blessed Sibanda <blessed15896@gmail.com>
2024-05-16 08:04:31 +0200

    The while loop

--- 01_CPP_Crash_Course.cpp
+++ 01_CPP_Crash_Course.cpp
@@ -1,30 +1,18 @@
 #include <print>;
-#include <array>;
 
 using namespace std;
 
 int main()
 {
     {
-        // Structured bindings with std::array.
-        array values{ 11, 22, 33 };
-		auto [x, y, z] {values};
-        println("x = {}, y = {}, z = {}\n", x, y, z);
+        // The while loop
+        int i{ 0 };
+        while (i < 5)
+        {
+            println("This is silly");
+            ++i;
+        }
     }
-    {
-        // Structured bindings with struct.
-        struct Point { double m_x, m_y, m_z; };
-        Point point;
-        point.m_x = 1.0; point.m_y = 2.0; point.m_z = 3.0;
-        auto [x, y, z] { point };
-        println("x = {}, y = {}, z = {}\n", x, y, z);
-    }
-    {
-        // Structured bindings with std::pair.
-        pair myPair{ "hello", 5 };
-        auto [theString, theInt] { myPair };
-        println("theString: {}", theString);
-        println("theInt: {}", theInt);
-    }
+    println("");
 }
 
