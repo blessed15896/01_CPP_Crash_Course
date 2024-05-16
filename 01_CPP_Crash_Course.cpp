db7d8edbdec126fca2075fcc57ad31eca4f8d9e2
Blessed Sibanda <blessed15896@gmail.com>
2024-05-16 07:58:28 +0200

    Structured bindings with struct

--- 01_CPP_Crash_Course.cpp
+++ 01_CPP_Crash_Course.cpp
@@ -9,7 +9,15 @@ int main()
         // Structured bindings with std::array.
         array values{ 11, 22, 33 };
 		auto [x, y, z] {values};
-        println("x = {}, y = {}, z = {}", x, y, z);
+        println("x = {}, y = {}, z = {}\n", x, y, z);
+    }
+    {
+        // Structured bindings with struct.
+        struct Point { double m_x, m_y, m_z; };
+        Point point;
+        point.m_x = 1.0; point.m_y = 2.0; point.m_z = 3.0;
+        auto [x, y, z] { point };
+        println("x = {}, y = {}, z = {}\n", x, y, z);
     }
 }
 
